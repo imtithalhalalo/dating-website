30c12570987a9643851cbbdc8a4c761e570f38c9
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-02 23:02:21 +0300

    backend: modify routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,16 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-Route::post('/signup', [AuthController::class, 'signup']);
-Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::group(["prefix"=> "v0.1"], function(){
+    Route::post('/signup', [AuthController::class, 'signup']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
 
