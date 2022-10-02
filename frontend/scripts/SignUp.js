98e12c75c402072a59cf7f5de46fba70f6793309
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-02 23:03:41 +0300

    frontend: add signup function axios

---
+++ frontend/scripts/SignUp.js
@@ -0,0 +1,47 @@
+export const SignUp = async (base_url) => {
+   
+    if (document.getElementById("signup")) {
+        document.getElementById("signup").addEventListener('click', async (e) => {
+            e.preventDefault();
+            const name = document.getElementById("name").value;
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+            const user_gender = document.querySelectorAll('input[name="user_gender"]');
+            const interested_gender = document.querySelectorAll('input[name="interested_gender"]');
+            const location = document.getElementById('location').value;
+            
+            let usergender;
+            const getGender = () => {
+                for (let i = 0; i < user_gender.length; i++) {
+                    if (user_gender[i].checked) {
+                        usergender = user_gender[i].value;
+                    }
+                }
+            }
+
+            let interestedgender;
+            const getInterestedGender = () => {
+                for (let i = 0; i < interested_gender.length; i++) {
+                    if (interested_gender[i].checked) {
+                        interestedgender = interested_gender[i].value;
+                    }
+                }
+            }
+
+            getGender();
+            getInterestedGender();
+
+                const url = base_url + "/signup";
+                const body = {
+                    name : name,
+                    email: email,
+                    password: password,
+                    gender: usergender,
+                    interested_in_gender: interestedgender,
+                    location: location
+                };
+
+                const user = await axios.post(url, body);
+        })
+    }
+}
