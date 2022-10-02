08c7a52389da078b21b5c3af76b67d4140afddd1
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-03 00:33:45 +0300

    backend: add login function

---
+++ frontend/scripts/Login.js
@@ -0,0 +1,26 @@
+export const Login = async (base_url) => {
+   
+    if (document.getElementById("login-btn")) {
+        document.getElementById("login-btn").addEventListener('click', async (e) => {
+            e.preventDefault();
+
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+
+            const url = base_url + "/login";
+            const data ={
+                email: email,
+                password: password
+            }
+
+            await axios.post(url, data).then(response=>{
+                if(response.data.message === "User successfully logged In!"){
+                    console.log(response.data.token.original.access_token);
+                    localStorage.setItem("token", response.data.token.original.access_token);
+                    window.location = "./index.html"
+                }
+            });
+            // console.log(user.data)
+        });
+    }
+}
