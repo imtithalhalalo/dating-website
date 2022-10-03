d525a8c219e8735d07e1cfc1012e5134cdb33ac7
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-03 19:14:36 +0300

    frontend: add user canvas

---
+++ frontend/scripts/User.js
@@ -0,0 +1,21 @@
+export const User = (user) => {
+    return (`
+    <div class="column" id="${user.id}">
+        <div class="image-user">
+            <img src="${user.image}" alt="" />
+        </div>
+
+        <div class="info">
+            <div class="info-details">
+                <h3>${user.name}</h3>
+                <br>
+                <div class="icons">
+                    <span id="${user.id}"><i class="fa fa-heart-o"></i></span>
+                    <span id="${user.id}"><i class="fa fa-comment-o"></i></span>
+                    <span id="${user.id}"><i class="fa fa-ban"></i></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    `)
+}
