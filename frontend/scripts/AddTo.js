c1e8e3206dc6da69027de6d16385b705603c6291
imtithalhalalo <halaloimtithal@gmail.com>
2022-10-04 16:56:45 +0300

    frontend: fix headers from token to Bearer

--- frontend/scripts/AddTo.js
+++ frontend/scripts/AddTo.js
@@ -1,15 +1,17 @@
+import { SendMessage } from "./SendMessage.js";
+const base_url = "http://127.0.0.1:8000/api/v0.1";
 export const AddTo = async (type, data) => {
 
     if(type == 'favorite'){
         const url = "http://127.0.0.1:8000/api/v0.1/addfavorite";
-        const response = await axios.post(url, data, { headers: {'Authorization': `token ${localStorage.getItem(`token`)}`}});
+        const response = await axios.post(url, data, { headers: {'Authorization': `Bearer ${localStorage.getItem(`token`)}`}});
         console.log(response);
         alert(response.data.message);
     }
 
     if(type == 'block'){
         const url = "http://127.0.0.1:8000/api/v0.1/addblocked";
-        const response = await axios.post(url, data, { headers: {'Authorization': `token ${localStorage.getItem(`token`)}`}});
+        const response = await axios.post(url, data, { headers: {'Authorization': `Bearer ${localStorage.getItem(`token`)}`}});
         console.log(response);
         alert(response.data.message);
     }
